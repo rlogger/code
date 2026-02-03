99d721a1e189d634f9e2439d68ec3b900ed9603a
rlogger <smsrajdeep@gmail.com>
2026-02-02 20:35:26 -0800

    add bfs solution

--- invert_binary_tree.py
+++ invert_binary_tree.py
@@ -22,3 +22,21 @@ class Solution:
     self.invertTree(root.right)
 
     return root
+
+
+# DFS solution
+# use the deque
+'''
+def invertTree(root):
+    if not root:
+        return None
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        node.left, node.right = node.right, node.left
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    return root
+'''
